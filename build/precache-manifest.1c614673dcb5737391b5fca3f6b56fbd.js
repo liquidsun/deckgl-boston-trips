976308be338549d6b3371f5eaf697b1549f349b3
Ivan Vasilyev <ivasilev86@gmail.com>
2020-01-29 12:28:56 +0100

    Add files via upload

---
+++ build/precache-manifest.1c614673dcb5737391b5fca3f6b56fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8769f81c2f3e7405b817603d7de0b97",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d83ab48d4647696e3696",
+    "url": "./static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "923a368fbd75280f5ccf",
+    "url": "./static/css/main.6d5feb54.chunk.css"
+  },
+  {
+    "revision": "d83ab48d4647696e3696",
+    "url": "./static/js/2.c3856a86.chunk.js"
+  },
+  {
+    "revision": "923a368fbd75280f5ccf",
+    "url": "./static/js/main.962b9ce3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
